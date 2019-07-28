1ffc9623b02a40ac2eb9cff7f2c3e07798015ea0
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-07-28 04:13:50 -0400

    add create method to PersonalNoteSerializer
    needed to grab the user from the request context in order to create a
    note

--- notes/api.py
+++ notes/api.py
@@ -6,6 +6,16 @@ from .models import PersonalNote
 
 
 class PersonalNoteSerializer(serializers.HyperlinkedModelSerializer):
+    def create(self, validated_data):
+        # import pdb
+        #
+        # pdb.set_trace()
+        # pass
+
+        user = self.context['request'].user
+        note = PersonalNote.objects.create(user=user, **validated_data)
+        return note
+
     class Meta:
         model = PersonalNote
         fields = ("title", "content")
